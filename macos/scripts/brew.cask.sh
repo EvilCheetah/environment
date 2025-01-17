5563ab9e1bf2c2214b36d6388224c08af94a9efd
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-01-17 17:38:06 -0600

    add(macos): additional casks and formulas

--- macos/scripts/brew.cask.sh
+++ macos/scripts/brew.cask.sh
@@ -1,6 +1,5 @@
 FILE=$(realpath "packages/brew.cask.env")
 
-echo $FILE
 
 # Check file
 if [ ! -f "$FILE" ]; then
@@ -8,4 +7,21 @@ if [ ! -f "$FILE" ]; then
     # exit 1
 fi
 
-cat $FILE
+
+# Iterate over `cask` applications in $FILE
+while IFS= read -r app || [ -n "$app" ]; do
+    # Skip empty lines and lines that start with `#`
+    if [ -z "$app" ] || [ "${app#\#}" != "$app" ]; then
+        continue
+    fi
+
+    echo "Installing cask: $app"
+    brew install --cask "$app"
+
+    # Check if the installation was successful
+    if [ $? -ne 0 ]; then
+        echo "Failed to install cask: $app"
+    else
+        echo "Successfully installed: $app"
+    fi
+done < "$FILE"
--- macos/scripts/brew.formula.sh
+++ macos/scripts/brew.formula.sh
@@ -1,11 +1,27 @@
 FILE=$(realpath "packages/brew.formula.env")
 
-echo $FILE
 
 # Check file
 if [ ! -f "$FILE" ]; then
-    echo "Cask file '$FILE' not found!"
+    echo "Formula file '$FILE' not found!"
     # exit 1
 fi
 
-cat $FILE
+
+# Iterate over `formula` applications in $FILE
+while IFS= read -r app || [ -n "$app" ]; do
+    # Skip empty lines and lines that start with `#`
+    if [ -z "$app" ] || [ "${app#\#}" != "$app" ]; then
+        continue
+    fi
+
+    echo "Installing formula: $app"
+    brew install "$app"
+
+    # Check if the installation was successful
+    if [ $? -ne 0 ]; then
+        echo "Failed to install formula: $app"
+    else
+        echo "Successfully installed: $app"
+    fi
+done < "$FILE"
