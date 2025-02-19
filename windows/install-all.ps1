4e864613259831128914d54d5ef2abd031fe2fad
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-02-19 15:37:54 -0600

    add(windows): todo to install gettext using `mingw`

--- windows/install-all.ps1
+++ windows/install-all.ps1
@@ -1,3 +1,7 @@
 & ./scripts/winget.install.ps1
 & ./scripts/golang.install.ps1
 & ./scripts/choco.install.ps1
+
+## TODO(eugene): Call all commands that require env to be set
+## Also, set all env variables for `mingw` and run the following:
+## `pacman -S mingw-w64-x86_64-gettext`
